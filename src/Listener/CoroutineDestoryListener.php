9344b61aea84aae4b109ddd3842bee1e7dfa163a
陈嘉豪 <271375247@qq.com>
2019-09-18 20:02:48 +0800

    first

---
+++ src/Listener/CoroutineDestoryListener.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+
+namespace cjh\Rabbitmq\Listener;
+
+
+use ReflectionException;
+use Swoft\Bean\BeanFactory;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\Event\Annotation\Mapping\Listener;
+use Swoft\Event\EventHandlerInterface;
+use Swoft\Event\EventInterface;
+use Swoft\SwoftEvent;
+
+/**
+ * Class CoroutineDestoryListener
+ *
+ * @since 2.0
+ *
+ * @Listener(event=SwoftEvent::COROUTINE_DESTROY)
+ */
+class CoroutineDestoryListener implements EventHandlerInterface
+{
+    /**
+     * @param EventInterface $event
+     *
+     * @throws ReflectionException
+     * @throws ContainerException
+     */
+    public function handle(EventInterface $event): void
+    {
+        /* @var ConnectionManager $conManager */
+        /*
+        $conManager = BeanFactory::getBean(ConnectionManager::class);
+        $conManager->release(true);
+        */
+    }
+}
